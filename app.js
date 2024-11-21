1761ef15a2261a43e78f12de88e9c7e1db590a24
Laman Hasanli <hesenlileman2001@gmail.com>
2024-11-21 23:52:51 +0100

    Add cors

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-require('dotenv').config(); // Load environment variables
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-const offerRoutes = require('./routes/offerRoutes'); // Import offer routes
-const cartRoutes = require('./routes/cartRoutes');
-const bidRoutes = require('./routes/bidroutes');
+require("dotenv").config(); // Load environment variables
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const offerRoutes = require("./routes/offerRoutes"); // Import offer routes
+const cartRoutes = require("./routes/cartRoutes");
+const bidRoutes = require("./routes/bidroutes");
 
 const app = express();
 const port = process.env.PORT || 3001;
+app.use(cors({ origin: true }));
 
 // Middleware for parsing JSON
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api', offerRoutes); // Add offer-related routes
-app.use('/api/cart', cartRoutes);
-app.use('/api/bid', bidRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api", offerRoutes); // Add offer-related routes
+app.use("/api/cart", cartRoutes);
+app.use("/api/bid", bidRoutes);
 
 // Start server
 app.listen(port, () => {
